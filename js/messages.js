85e4c1d6a22b069ca03f31e01bd7798b4d583092
310kent0 <bokuwazettainiryunennsimashen@gmail.com>
2016-10-14 20:47:42 +0900

    messages.jsのリファクタリング

--- js/messages.js
+++ js/messages.js
@@ -1,18 +1,16 @@
 function printMessage(messageFile, messageBox, nextButton, xmlHttpRequest, nextCommand) {
-	(function () {
-		function onClickNextButton() {
-			this.removeEventListener("click", onClickNextButton, false);
-			nextCommand();
-		}
-		
-		function onLoadMessage() {
-			xmlHttpRequest.removeEventListener("load", onLoadMessage, false);
-			nextButton.addEventListener("click", onClickNextButton, false);
-			messageBox.innerHTML = this.responseText;
-		}
-		
-		xmlHttpRequest.open("get", "messages/" + messageFile, true);
-		xmlHttpRequest.addEventListener("load", onLoadMessage, false);
-		xmlHttpRequest.send(null);
-	}());
+	var onClickNextButton = function() {
+		this.removeEventListener("click", onClickNextButton, false);
+		nextCommand();
+	}
+	
+	var onLoadMessage = function() {
+		xmlHttpRequest.removeEventListener("load", onLoadMessage, false);
+		nextButton.addEventListener("click", onClickNextButton, false);
+		messageBox.innerHTML = this.responseText;
+	}
+	
+	xmlHttpRequest.open("get", "messages/" + messageFile, true);
+	xmlHttpRequest.addEventListener("load", onLoadMessage, false);
+	xmlHttpRequest.send(null);
 }
